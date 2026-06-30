07b77555cc2a9c8f2c2dc67599a93a96cadaf8a1
sumesh <sumesh@frontlinetech.in>
2026-06-30 16:55:55 +0530

    update

--- src/components/home/products.jsx
+++ src/components/home/products.jsx
@@ -130,7 +130,7 @@ const productData = [
 ];
 
 const Products = () => {
-  const whatsappNumber = "911234567840";
+  const whatsappNumber = "919876543210";
 
   const sendWhatsapp = (product) => {
     const message = `Hello, I'm interested in ${product.name}. Please share more details.`;
